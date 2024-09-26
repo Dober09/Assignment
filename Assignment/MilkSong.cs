252801d676dc623c1e9bffea594289adbf735851
Dober09 <lebohanglehutjo@gmail.com>
2024-09-26 12:01:26 +0200

    Add project files.

---
+++ Assignment/MilkSong.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class MilkSong
+    {
+
+      public string Lyric(int number)
+        {
+            string lyric = number+" bottles of milk on the wall, "+ number +" bottles of milk. Take one down and pass it around, \r\n";
+
+
+              return lyric;
+         }
+
+        public void Song(int number)
+        {
+            while(number > 1)
+            {
+                number--;
+
+                if (number == 1)
+                {
+                    Console.WriteLine("1 bottle of milk on the wall, 1 bottle of milk. Take it and drink it");
+                }
+                else
+                {
+                      Console.WriteLine(Lyric(number));
+                    
+
+                }
+            }
+        }
+    }
+
+}
+
---
+++ Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MilkSong song = new MilkSong();
+
+            song.Song(100);
+          
+
+            
+
+        }
+    }
+}
